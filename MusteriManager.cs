f576ae1a6475525affe1d03a228a51cb54892459
Nobody034 <yusufekin034@hotmail.com>
2021-10-17 15:17:35 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.Write("Adınızı giriniz --> ");
+            musteri.Ad = Console.ReadLine();
+
+            Console.Write("Soyadını giriniz --> ");
+            musteri.Soyad = Console.ReadLine();
+
+            Console.Write("Id numaranızı giriniz --> ");
+            musteri.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Telefonunuzu giriniz --> ");
+            musteri.Telefon = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Adresinizi giriniz --> ");
+            musteri.Adress = Console.ReadLine();
+        }
+
+        public void MusteriListele(List<Musteri> musteriler)
+        {
+            int i = 0;
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müsteri[{0}]", i++);
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Müsteri Adı: " + musteri.Ad);
+                Console.WriteLine("Müsteri Soyadı: " + musteri.Soyad);
+                Console.WriteLine("Müsteri Id Numarası: " + musteri.Id);
+                Console.WriteLine("Müsteri Telefonu: " + musteri.Telefon);
+                Console.WriteLine("Müsteri Adresi: " + musteri.Adress);
+                Console.WriteLine();
+            }
+        }
+
+        public void MusteriSil(List<Musteri> musteriler)
+        {
+            int idNumara = 0;
+
+            Console.Write("Silmek istediğiniz müşterinin ıd numarasını giriniz --> ");
+            idNumara = Convert.ToInt32(Console.ReadLine());
+
+            int musteriSayisi = musteriler.Count;
+
+            for (int i = 0; i < musteriSayisi; i++)
+            {
+                if (idNumara == musteriler[i].Id)
+                {
+                    Console.WriteLine(musteriler[i].Id + " numaralı müşteri başarıyla silindi");
+                    musteriler.RemoveAt(i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Bu numaraya ait müşteri yok!!!");
+                }
+            }
+
+            //foreach (Musteri musteri in musteriler)
+            //{
+            //    if (idNumara == musteri.Id)
+            //    {
+            //        musteriler.RemoveAt(i);
+            //        Console.WriteLine(musteri.Id + " numaralı müşteri başarıyla silindi");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bu numaraya ait müşteri yok!!!");
+            //    }
+            //}
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        public static List<Musteri> musteriler = new List<Musteri>();
+        public static int sayac = 0;
+
+        static void Main(string[] args)
+        {
+            girisEkrani();
+        }
+
+        static void girisEkrani()
+        {
+
+            Console.WriteLine(
+            @"[B]======================[KAYIT SİSTEMİ]=====================[-][o][x]
+|                                                                    |
+|                Kayıt Sistemine Hoşgeldiniz!                        |
+|                                                                    |
+|                   Müşteri Eklemek İçin       (1)                   |
+|                   Müşteri Silmek İçin        (2)                   |
+|              Müşterileri Görüntülemek İçin   (3)                   |
+|                        Çıkış                 (4)                   |
+|                                                                    |
+|====================================================================|"
+            );
+            Console.WriteLine();
+            int numara = girisKontrol();
+            secimSonuc(numara);
+        }
+        static int girisKontrol()
+        {
+            char secim;
+
+            do
+            {
+                Console.Write("Giriniz --> ");
+                secim = Console.ReadKey().KeyChar;
+
+                if (49 <= secim && secim <= 52)
+                {
+                    Console.WriteLine();
+                    return Convert.ToInt32(secim.ToString());
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            } while (true);
+        }
+
+        static void secimSonuc(int secim)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+
+            switch (secim)
+            {
+                case 1:
+                    Console.WriteLine();
+                    Musteri musteri = new Musteri();
+                    musteriler.Add(musteri);
+                    musteriManager.MusteriEkle(musteri);
+                    cikisKontrol();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    musteriManager.MusteriSil(musteriler);
+                    cikisKontrol();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    musteriManager.MusteriListele(musteriler);
+                    cikisKontrol();
+                    break;
+                case 4:
+                    Console.WriteLine("Çıkış yapılıyor...");
+                    break;
+                default:
+                    Console.WriteLine("Çıkış yapılıyor...");
+                    break;
+            }
+        }
+
+        static void cikisKontrol()
+        {
+            Console.WriteLine();
+            Console.Write("Giris Ekranı için (1)\nÇıkış için (2)\n\ngiriniz --> ");
+
+            if (Convert.ToInt32(Console.ReadLine()) == 1)
+            {
+                Console.WriteLine();
+                girisEkrani();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Çıkış yapılıyor...");
+            }
+        }
+    }
+}
